903f7951ffb4e825c21a22223281341b7d3fbea5
yaoinformatics <37345096+Yaoyx@users.noreply.github.com>
2020-06-12 13:55:01 -0400

    Update phylogeny.py

--- fastclone/phylogeny.py
+++ fastclone/phylogeny.py
@@ -1,61 +1,74 @@
 """Subclonal phylogeny inference"""
 
-import logbook
-import numpy
-import pandas
-import scipy.misc
-import scipy.optimize
-import scipy.signal
-import scipy.stats
+import networkx as nx
+import random
+import matplotlib.pyplot as plt
+from collections import Counter
+import numpy as np
 
+def infer(subclones, score, output):
+    G = nx.Graph()
 
-_LOG = logbook.Logger(__name__)
+    if len(subclones) > 1:
+        root = 0
+        edge_ls = [(root, 1)]
 
+        if len(subclones) > 2:
+            subclone_snv_number = Counter([np.nanargmax(row) for row in subclones])
+            i = 2
+            while i != len(subclones):
+                if subclone_snv_number[i] < subclone_snv_number[i - 1]:
+                    edge_ls.append((root, i))
+                else:
+                    edge_ls.append((i - 1, i))
+                    root = i - 1
+                i += 1
 
-def infer(subclones):
-    subclones = subclones[::-1]
-    _LOG.info("Phylogeny inference model: Beta distribution")
-    parents = numpy.zeros(subclones.size, dtype="i1")
-    parents[0] = 0
-    parents[1] = 0
-    if parents.size < 3:
-        _LOG.debug("Phylogeny: {}", parents)
-        return (parents.size - parents - 1)[::-1]
-    fractions = numpy.zeros(parents.shape, dtype="f4")
-    leftover = numpy.copy(subclones)
-    fractions[0] = leftover[0]
-    fractions[1] = leftover[1] / leftover[0]
-    leftover[0] -= leftover[1]
-    parents[2] = _assign_3rd_subclone(leftover, fractions)
-    for j in range(3, parents.size):
-        parents[j] = _assign(j, leftover, fractions)
-    _LOG.debug("Phylogeny: {}", parents + 1)
-    return (parents.size - parents - 1)[::-1]
+        G.add_edges_from(edge_ls)
 
-def _assign_3rd_subclone(leftover, fractions):
-    if leftover[0] > leftover[2] + 0.1:
-        fractions[2] = leftover[2] / leftover[0]
-        leftover[0] -= leftover[2]
-        return 0
     else:
-        fractions[2] = leftover[2] / leftover[1]
-        leftover[1] -= leftover[2]
-        return 1
+        G.add_nodes_from([0])
 
-def _assign(idx, leftover, fractions):
-    check = leftover[idx] < leftover[:idx]
-    new_frac = leftover[idx] / leftover[check]
-    fracs = numpy.hstack((
-        numpy.repeat(fractions[:idx, None], new_frac.size, axis=1),
-        new_frac,
-    ))
-    likelihood = numpy.asarray([
-        scipy.stats.beta(
-            *scipy.stats.beta.fit(fracs[:, j], f0=1, floc=0, fscale=1)
-        ).logpdf(fracs[:, j]).sum()
-        for j in range(new_frac.size)
-    ])
-    parent = numpy.where(check)[0][likelihood.argmax()]
-    fractions[idx] = leftover[idx] / leftover[parent]
-    leftover[parent] -= leftover[idx]
-    return parent
+    pos = hierarchy_pos(G, root=0)
+    nx.draw(G, pos=pos, with_labels=True, node_color='orange', alpha=0.7)
+    plt.savefig(output + 'phylogeny.png')
+
+
+def hierarchy_pos(G, root=None, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5):
+    if not nx.is_tree(G):
+        raise TypeError('cannot use hierarchy_pos on a graph that is not a tree')
+
+    if root is None:
+        if isinstance(G, nx.DiGraph):
+            root = next(iter(nx.topological_sort(G)))  #allows back compatibility with nx version 1.11
+        else:
+            root = random.choice(list(G.nodes))
+
+    def _hierarchy_pos(G, root, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5, pos = None, parent = None):
+        '''
+        see hierarchy_pos docstring for most arguments
+
+        pos: a dict saying where all nodes go if they have been assigned
+        parent: parent of this branch. - only affects it if non-directed
+
+        '''
+
+        if pos is None:
+            pos = {root:(xcenter,vert_loc)}
+        else:
+            pos[root] = (xcenter, vert_loc)
+        children = list(G.neighbors(root))
+        if not isinstance(G, nx.DiGraph) and parent is not None:
+            children.remove(parent)
+        if len(children)!=0:
+            dx = width/len(children)
+            nextx = xcenter - width/2 - dx/2
+            for child in children:
+                nextx += dx
+                pos = _hierarchy_pos(G,child, width = dx, vert_gap = vert_gap,
+                                    vert_loc = vert_loc-vert_gap, xcenter=nextx,
+                                    pos=pos, parent = root)
+        return pos
+
+
+    return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
